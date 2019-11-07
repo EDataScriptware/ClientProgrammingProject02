e76ad6dd32551a649da8b75bf1460eff89a4ff47
emr9018 <emr9018@rit.edu>
2019-11-07 10:52:54 -0500

    Leaving a broken plug-in jQuery

--- jQuery_plugins.js
+++ jQuery_plugins.js
@@ -3,12 +3,16 @@
 // TYPE-WRITER
 $(document).ready(function(){
 
+
+
 // Header type writer plug-in
 $('#headerTitle').typeOut(); 
-// SHOW AND HIDE TABLES in an orderly fashion manner
+
+// SHOW AND HIDE TABLES in an orderly fashion manner - not functioning 
+// 							| Just going to move on
 		//To hide the menu
 		$("#hidediv").click(function () {
-			alert("I am being clicked!");
+			console.log("I am being clicked!");
 		$(".men_ex").hide("fast",function(){
 		alert("Menu is hidden!");
 		});
@@ -17,11 +21,12 @@ $('#headerTitle').typeOut();
 
 		//To show the menu
 		$("#showdiv").click(function () {
-		alert("I am being clicked!");
+		console.log("I am being clicked!");
 		$(".men_ex").show(2000,function(){
 		alert("Menu is displayed!");
 		});
-	});  
+	});
+
 
 });
 
